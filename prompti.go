24f34af0086d353135aff74b775a1cc1a79a5c65
indaco <indaco@users.noreply.github.com>
2026-03-17 17:17:49 +0100

    refactor: export sentinel errors and add package documentation

--- prompti.go
+++ prompti.go
@@ -1,1 +1,11 @@
 package prompti
+
+import "errors"
+
+var (
+	// ErrCancelled is returned when the user cancels the prompt (ctrl+c, esc).
+	ErrCancelled = errors.New("prompti: operation cancelled")
+
+	// ErrEmpty is returned when the user submits empty input.
+	ErrEmpty = errors.New("prompti: empty input")
+)
